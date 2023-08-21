3a8b5ca1cc2a5f026398938323b0a1009998ee59
Tuğçe Tuğçe <etugceorge@gmail.com>
2023-08-22 01:12:47 +0300

    Caesar encryption
    Caesar encryption algorithm with c# webform

---
+++ Default.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register assembly="Telerik.Web.UI" namespace="Telerik.Web.UI" tagprefix="telerik" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN">
+
+<html>
+<head runat="server">
+<link rel="Stylesheet" href="Imparator.css" />
+<title>Sezar</title>
+    <style type="text/css">
+        .kutu {}
+        #form1 {
+            height: 930px;
+            width: 893px;
+            margin-right: 0px;
+        }
+        .auto-style1 {
+            width: 176px;
+        }
+        .auto-style3 {
+            width: 176px;
+            height: 42px;
+        }
+        .auto-style5 {
+            height: 42px;
+        }
+        .auto-style8 {
+            height: 40px;
+        }
+        .auto-style11 {
+            width: 176px;
+            height: 40px;
+        }
+        .auto-style12 {
+            margin-left: 0px;
+        }
+    </style>
+</head>
+<body>
+<form id="form1" runat="server">
+<asp:Label ID="Label_kullanici" runat="server" Visible="False"></asp:Label>
+    <br />
+    <br />
+    <table style="width: 100%; height: 262px;">
+        <tr>
+            <td colspan="3">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style1">
+                <asp:Label ID="Label1" runat="server" Enabled="False" Font-Size="Small" Text="Metin:" Font-Bold="False"></asp:Label>
+            </td>
+            <td colspan="2">
+                <asp:TextBox ID="T_metin" runat="server" Height="97px" TextMode="MultiLine" Width="688px" OnTextChanged="T_metin_TextChanged"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">&nbsp;</td>
+        </tr>
+        <tr>
+            <td class="auto-style3">
+                <asp:Label ID="Label2" runat="server" Text="Sayı" Font-Bold="False" Font-Size="Small"></asp:Label>
+            </td>
+            <td class="auto-style5">
+                <asp:TextBox ID="T_oteleme_sayisi" runat="server" OnTextChanged="T_oteleme_sayisi_TextChanged" AutoPostBack="True"></asp:TextBox>
+            </td>
+            <td class="auto-style5">
+                <asp:Button ID="B_sifrele" runat="server" CssClass="auto-style12" Text="Hesapla" OnClick="B_sifrele_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style8" colspan="3"></td>
+        </tr>
+        <tr>
+            <td class="auto-style11">
+                <asp:Label ID="Label3" runat="server" Font-Size="Small" Text="Şifreli Metin" Font-Bold="False"></asp:Label>
+            </td>
+            <td class="auto-style8" colspan="2">
+                <asp:TextBox ID="T_sonuc_metin" runat="server" Height="96px" TextMode="MultiLine" Width="692px"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td class="auto-style8" colspan="3"></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading;
+using System.Globalization;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Drawing;
+using System.Net.Mail;
+using System.IO;
+using System.Reflection;
+using System.Diagnostics;
+using System.Net;
+using System.Design;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Web.SessionState;
+using System.Drawing.Imaging;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using Telerik.Web.UI;
+using OfficeOpenXml;
+using System.Drawing.Text;
+using System.Drawing.Design;
+using BarcodeLib.Barcode;
+using DataMatrix.net;
+using MessagingToolkit.QRCode;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using KeepAutomation.Barcode;
+using KeepAutomation.Barcode.Bean;
+using KeepAutomation.Barcode.Web;
+using System.Drawing.Printing;
+public partial class _Default : System.Web.UI.Page
+{
+    Test_DataClassesDataContext vt_test = new Test_DataClassesDataContext();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Response.Buffer = true;
+        Response.CacheControl = "no-cache";
+        if (Label_kullanici.Text == "")
+        {
+            Label_kullanici.Text = "Mirror";
+            Label_kullanici.Visible = false;
+        }
+    }
+    string metin = "";
+    int oteleme_sayisi;
+    string sifreli_metin;
+    string alfabe = "abcçdefgğhıijklmnoöprsştuüvyz";
+    void sifrele()
+    {
+        metin = T_metin.Text.ToLower();
+        oteleme_sayisi = Convert.ToInt32(T_oteleme_sayisi.Text);
+        if (oteleme_sayisi > 0)
+        {
+            for (int i = 0; i < metin.Length; i++)
+            {
+                bool sonuc = false;
+                for (int j = 0; j < alfabe.Length; j++)
+                {
+                    if (metin[i] == alfabe[j])
+                    {
+                        sonuc = true;
+                        char karakter = alfabe[(j + oteleme_sayisi) % 29];
+                        sifreli_metin += karakter;
+                    }
+                }
+                if (!sonuc)
+                {
+                    sifreli_metin += metin[i];
+                }
+            }
+        }
+        T_sonuc_metin.Text = "";
+        T_sonuc_metin.Text = sifreli_metin;
+    }
+    protected void T_metin_TextChanged(object sender, EventArgs e)
+    {
+    }
+    protected void T_oteleme_sayisi_TextChanged(object sender, EventArgs e)
+    {
+        try
+        {
+            int kontrol = Convert.ToInt32(T_oteleme_sayisi.Text);
+        }
+        catch
+        {
+            T_oteleme_sayisi.Text = "0";
+        }
+    }
+    protected void B_sifrele_Click(object sender, EventArgs e)
+    {
+        sifrele();
+    }
+}
